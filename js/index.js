bc99c86b492ed24ff0024c4f1c994951830e806f
lolkagirl <127850230+lolkagirl@users.noreply.github.com>
2024-02-01 03:39:21 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,40 +1,30 @@
-var currentAreaIndex = 1;
-var areas = document.querySelectorAll('.text-area');
-
-function showNextArea() {
-    if (currentAreaIndex < areas.length) {
-        areas[currentAreaIndex - 1].classList.add('hidden');
-        areas[currentAreaIndex].classList.remove('hidden');
-        currentAreaIndex++;
-    } else {
-        // Если достигнут конец, возвращаемся к началу
-        currentAreaIndex = 1;
-        // Показываем первую область, а остальные скрываем
-        for (var i = 0; i < areas.length; i++) {
-            if (i === 0) {
-                areas[i].classList.remove('hidden');
-            } else {
-                areas[i].classList.add('hidden');
-            }
-        }
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+      // удалим у кнопки класс btn-up_hide
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      // добавим к кнопке класс btn-up_hide
+      this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+      // при прокрутке содержимого страницы
+      window.addEventListener('scroll', () => {
+        // определяем величину прокрутки
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      // при нажатии на кнопку .btn-up
+      document.querySelector('.btn-up').onclick = () => {
+        // переместим в начало страницы
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
     }
-}
-
-function showPreviousArea() {
-    if (currentAreaIndex > 1) {
-        areas[currentAreaIndex - 1].classList.add('hidden');
-        areas[currentAreaIndex - 2].classList.remove('hidden');
-        currentAreaIndex--;
-    } else {
-        // Если достигнут начало, переходим к концу
-        currentAreaIndex = areas.length;
-        // Показываем последнюю область, а остальные скрываем
-        for (var i = 0; i < areas.length; i++) {
-            if (i === areas.length - 1) {
-                areas[i].classList.remove('hidden');
-            } else {
-                areas[i].classList.add('hidden');
-            }
-        }
-    }
-}
+  }
+  btnUp.addEventListener();
